0f0f5a551b1fa18ecd68749c9061da8b608d44ce
vasilenko2000 <ks2974070>
2014-06-02 23:48:02 +0300

    + Sidebar and cities section

---
+++ js/jquery.main.js
@@ -0,0 +1,10 @@
+// init
+jQuery.noConflict();
+;(function($) {
+    $(function() {
+        Cufon.replace('.cities .caption a', {
+            color: '-linear-gradient(#63afde, #3f70c4)',
+            textShadow: '1px 2px #999'
+        });
+    });
+}(jQuery));
